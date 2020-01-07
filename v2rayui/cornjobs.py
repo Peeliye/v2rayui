f881c89e09cd136acd1101d8024351756d996b37
yunwei <hiyunwei@gmail.com>
2020-01-07 09:51:17 +0800

    Add v2ray_api submodule

--- v2rayui/cornjobs.py
+++ v2rayui/cornjobs.py
@@ -1,7 +1,7 @@
 from v2rayui.models import User, InviteCode, Node
 
 
-def organize_vmss_users():
+def organize_vmess_users():
     try:
         users = User.objects.filter().order_by("-date_joined")
     except User.DoesNotExist:
